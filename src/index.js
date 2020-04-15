55c009101e95777289b4985c7c9399a3f4b44ba3
Regulyarniy Nikolay <regularniy@gmail.com>
2020-04-15 14:34:46 +1000

    travis deploy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import React from 'react';
-import { store, history} from './store';
+import { store, history, createHistory} from './store';
 
 import { Route, Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
@@ -10,7 +10,7 @@ import App from './components/App';
 
 ReactDOM.render((
   <Provider store={store}>
-    <ConnectedRouter history={history} basename={`/react-redux-realworld-example-app`}>
+    <ConnectedRouter history={history}>
       <Switch>
         <Route path="/" component={App} />
       </Switch>
--- src/store.js
+++ src/store.js
@@ -7,7 +7,7 @@ import reducer from './reducer';
 import { routerMiddleware } from 'react-router-redux'
 import createHistory from 'history/createBrowserHistory';
 
-export const history = createHistory();
+export const history = createHistory({ basename: `/react-redux-realworld-example-app` });
 
 // Build the middleware for intercepting and dispatching navigation actions
 const myRouterMiddleware = routerMiddleware(history);
